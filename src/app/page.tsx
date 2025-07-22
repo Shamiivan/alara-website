f8b8bfb50ed43a91b4711f9c12dfbe6d5f22c15d
shamiivan <shamiivan@gmail.com>
2025-07-22 10:34:32 -0400

    feat: add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,22 @@ import Image from "next/image";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import Hero from "@/components/landing/Hero"
+import Features from "@/components/landing/Features"
+import DemoSection from "@/components/landing/DemoSection"
+import WhoThisIsFor from "@/components/landing/WhoThisIsFor"
+import Vision from "@/components/landing/Vision"
+
 export default function Home() {
   const tasks = useQuery(api.tasks.get_tasks);
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    <div className="font-sans items-center justify-items-center">
+      <main className="flex flex-col">
+        {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
         <Hero />
+        <Vision />
+        <Features />
+        <WhoThisIsFor />
+
       </main>
     </div>
   );
