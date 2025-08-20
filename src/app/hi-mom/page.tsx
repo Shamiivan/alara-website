ea3d42c60f3d02e1b930e58ccb164982eae2e929
shamiivan <shamiivan@gmail.com>
2025-08-20 10:19:16 -0400

    refactor: remove HiMom page component

--- src/app/hi-mom/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HiMom() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-purple-500">
-      <h1 className="text-6xl font-bold text-white mb-4">Hi mom</h1>
-      <p className="text-xl text-white">This page was created just for you!</p>
-    </div>
-  );
-}
