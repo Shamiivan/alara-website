352e4e62ae81a225d0721b1bc0b399f93a5268df
shamiivan <shamiivan@gmail.com>
2025-08-20 09:39:28 -0400

    feat: added backend support for redirects

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,4 +3,11 @@ import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Google],
+  callbacks: {
+    async redirect({ redirectTo }) {
+      // get the base URL of the site from the env
+      const baseUrl = process.env.SITE_URL || "http://localhost:3000";
+      return `${baseUrl}${redirectTo}`;
+    }
+  }
 });
