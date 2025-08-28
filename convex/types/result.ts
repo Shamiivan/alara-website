fc0bf951f7939ebc02cda2ef5cedb0ee97300858
shamiivan <shamiivan@gmail.com>
2025-08-27 20:30:21 -0400

    update the result function

--- convex/types/result.ts
+++ convex/types/result.ts
@@ -1,3 +1,11 @@
 export type Result<T> =
   | { success: true; data: T }
-  | { success: false; error: string; };
+  | { success: false; error: string };
+
+export function Ok<T>(data: T): Result<T> {
+  return { success: true, data };
+}
+
+export function Err<T>(error: string): Result<T> {
+  return { success: false, error };
+}
